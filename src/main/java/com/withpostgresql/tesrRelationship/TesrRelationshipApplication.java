32b15330f93fd67ca3617740f8ac9388a1909129
Emrah1998 <emrahqarawov240@gmail.com>
2024-11-28 12:52:39 +0400

    withPostgre

---
+++ src/main/java/com/withpostgresql/tesrRelationship/TesrRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.withpostgresql.tesrRelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesrRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesrRelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/withpostgresql/tesrRelationship/TesrRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withpostgresql.tesrRelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesrRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
